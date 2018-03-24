f838eddbf4d39b834e92ac76a9e1c12e51d8db58
Johannes Reuter <jreuter@inovex.de>
2018-03-24 16:00:28 +0100

    embed base64-arraybuffer into loader to embed as base64-string

--- index.js
+++ index.js
@@ -22,13 +22,35 @@ module.exports = function(buffer) {
     }
   }
 
-  var out = "var buffer = new ArrayBuffer(" + buffer.length + ");";
-  out += "var uint8 = new Uint8Array(buffer);";
-  out += "uint8.set([";
-  for(var i = 0; i < buffer.length; i++) {
-    out += buffer[i] + ","
+  var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+  // Use a lookup table to find the index.
+  var lookup = new Uint8Array(256);
+  for (var i = 0; i < chars.length; i++) {
+    lookup[chars.charCodeAt(i)] = i;
   }
-  out += "]);"
+
+  function _arrayBufferToBase64(arraybuffer) {
+    var bytes = new Uint8Array(arraybuffer),
+    i, len = bytes.length, base64 = "";
+
+    for (i = 0; i < len; i+=3) {
+      base64 += chars[bytes[i] >> 2];
+      base64 += chars[((bytes[i] & 3) << 4) | (bytes[i + 1] >> 4)];
+      base64 += chars[((bytes[i + 1] & 15) << 2) | (bytes[i + 2] >> 6)];
+      base64 += chars[bytes[i + 2] & 63];
+    }
+
+    if ((len % 3) === 2) {
+      base64 = base64.substring(0, base64.length - 1) + "=";
+    } else if (len % 3 === 1) {
+      base64 = base64.substring(0, base64.length - 2) + "==";
+    }
+
+    return base64;
+  };
+
+  var out = `var buffer = "${_arrayBufferToBase64(buffer)}";`;
   out += bootstrap;
 
   this.callback(null, out);
